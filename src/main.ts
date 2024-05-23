e9888494e65a039ea7ba47ce6eb1c68b795390c3
1dylanleray <dleray77@outlook.fr>
2024-05-23 20:08:29 +0200

    fix(db): fix bugs of db connection

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+    console.log(process.env.DATABASE_PASSWORD);
     const app = await NestFactory.create(AppModule);
     await app.listen(3000);
 }
